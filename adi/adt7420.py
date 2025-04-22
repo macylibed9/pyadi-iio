651c6b6829020173d12a01e3c938abbb61122c5a
Macy Libed <macy.libed@analog.com>
2025-04-22 10:27:31 +0800

    Adding original comments
    Signed-off-by: Macy Libed <macy.libed@analog.com>

--- adi/adt7420.py
+++ adi/adt7420.py
@@ -40,7 +40,7 @@ class adt7420(attribute, context_manager):
                 raise Exception(f"Unsupported: {name}")
 
     class channel_temp(attribute):
-        """Channel for no-OS driver"""
+        """ADT7420 Channel for no-OS"""
 
         def __init__(self, ctrl, channel_name):
             self._ctrl = ctrl
@@ -48,43 +48,51 @@ class adt7420(attribute, context_manager):
 
         @property
         def temp_val(self):
-            """ Read temperature value """
+            """ ADT7420 Channel Temperature Value """
             return self._get_iio_attr(self.name, "temp", False)
 
         @property
         def temp_max(self):
+            """ ADT7420 Channel Max Temperature """
             return self._get_iio_attr(self.name, "temp_max", False)
 
         @temp_max.setter
         def temp_max(self, value):
+            """ ADT7420 Channel Max Temperature """
             return self._set_iio_attr(self.name, "temp_max", False, value)
 
         @property
         def temp_min(self):
+            """ ADT7420 Channel Min Temperature """
             return self._get_iio_attr(self.name, "temp_min", False)
 
         @temp_min.setter
         def temp_min(self, value):
+            """ ADT7420 Channel Min Temperature """
             return self._set_iio_attr(self.name, "temp_min", False, value)
 
         @property
         def temp_crit(self):
+            """ ADT7420 Channel Critical Temperature """
             return self._get_iio_attr(self.name, "temp_crit", False)
 
         @temp_crit.setter
         def temp_crit(self, value):
+            """ ADT7420 Channel Critical Temperature """
             return self._set_iio_attr(self.name, "temp_crit", False, value)
 
         @property
         def temp_hyst(self):
+            """ ADT7420 Channel Hysteresis Temperature """
             return self._get_iio_attr(self.name, "temp_hyst", False)
 
         @temp_hyst.setter
         def temp_hyst(self, value):
+            """ ADT7420 Channel Hysteresis Temperature """
             return self._set_iio_attr(self.name, "temp_hyst", False, value)
 
     class channel_temp1(attribute):
-        """Channel for Linux driver"""
+        """ADT7420 Channel for Linux"""
 
         def __init__(self, ctrl, channel_name):
             self._ctrl = ctrl
@@ -92,36 +100,45 @@ class adt7420(attribute, context_manager):
 
         @property
         def temp_val(self):
+            """ ADT7420 Channel Temperature Value """
             return self._get_iio_attr(self.name, "input", False)
 
         @property
         def temp_max(self):
+            """ ADT7420 Channel Max Temperature """
             return self._get_iio_attr(self.name, "max", False)
 
         @temp_max.setter
         def temp_max(self, value):
+            """ ADT7420 Channel Max Temperature """
             return self._set_iio_attr(self.name, "max", False, value)
 
         @property
         def temp_min(self):
+            """ ADT7420 Channel Min Temperature """
             return self._get_iio_attr(self.name, "min", False)
 
         @temp_min.setter
         def temp_min(self, value):
+            """ ADT7420 Channel Min Temperature """
             return self._set_iio_attr(self.name, "min", False, value)
 
         @property
         def temp_crit(self):
+            """ ADT7420 Channel Critical Temperature """
             return self._get_iio_attr(self.name, "crit", False)
 
         @temp_crit.setter
         def temp_crit(self, value):
+            """ ADT7420 Channel Critical Temperature """
             return self._set_iio_attr(self.name, "crit", False, value)
 
         @property
         def temp_hyst(self):
+            """ ADT7420 Channel Hysteresis Temperature """
             return self._get_iio_attr(self.name, "max_hyst", False)
 
         @temp_hyst.setter
         def temp_hyst(self, value):
+            """ ADT7420 Channel Hysteresis Temperature """
             return self._set_iio_attr(self.name, "max_hyst", False, value)
